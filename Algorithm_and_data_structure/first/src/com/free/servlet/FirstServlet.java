3ad2c263a9105c8086a6b97edc568e81db3a03fd
Faronsince2016 <lwchupin@163.com>
2017-03-12 14:20:20 +0800

    3.6

---
+++ Algorithm_and_data_structure/first/src/com/free/servlet/FirstServlet.java
@@ -0,0 +1,20 @@
+package com.free.servlet;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Faron on 2017/3/12.
+ */
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int i = 0;
+        i++;
+        System.out.println("My first servlet! +"+i);
+    }
+}

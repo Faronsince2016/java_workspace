a8c4fcc612e8aeba7cc7f846884522104333e925
Faronsince2016 <lwchupin@163.com>
2017-03-12 17:13:38 +0800

    service 调用顺序

---
+++ JavaWeb/src/com/free/servlet/TestService.java
@@ -0,0 +1,40 @@
+package com.free.servlet;
+
+/**
+ * Created by Faron on 2017/3/12.
+ */
+public class TestService {
+
+
+    void service(){
+        System.out.println("父类的service被调用");
+        doGet();   //这里的doGet调用的不是父类的doGet   而是子类的
+                   //父类的service是被子类所调用的  所以这里的doGet是在子类的内存中的  要想调用父类的  这里必须使用super.doGet() 
+    }
+
+    void doGet(){
+        System.out.println("父类的doGet被调用!");
+    }
+}
+
+class ChildrenService extends TestService{
+    @Override
+    void doGet() {
+        System.out.println("子类的doGet被调用");
+
+    }
+
+    @Override
+    void service() {
+        System.out.println("子类的service被调用！");
+        super.service();
+    }
+
+}
+
+class  TestMain{
+    public  static  void main(String[] args){
+        ChildrenService test = new ChildrenService();
+        test.service();
+    }
+}
